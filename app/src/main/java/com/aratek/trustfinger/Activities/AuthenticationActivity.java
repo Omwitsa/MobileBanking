ff36c13e155006bc384248230aec381b3f549c76
Kemboi <kemboihillz@gmail.com>
2020-10-01 10:49:26 +0300

    enroll module

---
+++ app/src/main/java/com/aratek/trustfinger/Activities/AuthenticationActivity.java
@@ -0,0 +1,44 @@
+package com.aratek.trustfinger.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.aratek.trustfinger.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AuthenticationActivity extends AppCompatActivity {
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.btnSignup)
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        ButterKnife.bind(this);
+
+        btnSignup.setEnabled(false);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FingeprintActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+//        btnSignup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), FingeprintActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
